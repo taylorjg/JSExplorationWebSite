8834bef025ffb4274b9e9e9213d6322bdc79dbeb
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-04-18 14:28:33 +0100

    Added running of Jasmine unit tests to the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,14 +13,26 @@ module.exports = function (grunt) {
 				"ObjectCreation/*.js"
 			]
         },
+        jasmine: {
+            all_targets: {
+                src: [
+                    "Underscore/underscore.js",
+                    "ObjectCreation/ObjectCreation.js"
+                ],
+                options: {
+                    specs: "ObjectCreation/*Spec.js"
+                }
+            }
+        },
         watch: {
             files: ["<%= jshint.files %>"],
-            tasks: ["jshint"]
+            tasks: ["jshint", "jasmine"]
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
 
-    grunt.registerTask("default", ["jshint"]);
+    grunt.registerTask("default", ["jshint", "jasmine"]);
 };
